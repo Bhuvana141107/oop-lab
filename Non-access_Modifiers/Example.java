44841c702c669f970289e2d22e252895119e9624
Bhuvana141107 <bhuvanaranidatti@gmail.com>
2026-03-07 19:41:20 +0530

    Add files via upload

---
+++ Non-access_Modifiers/Example.java
@@ -0,0 +1,12 @@
+class Example {
+
+    static int count = 10;
+
+    static void display() {
+        System.out.println("Count: " + count);
+    }
+
+    public static void main(String[] args) {
+        Example.display();
+    }
+}
---
+++ Non-access_Modifiers/FinalExample.java
@@ -0,0 +1,9 @@
+class FinalExample {
+
+    final int number = 50;
+
+    public static void main(String[] args) {
+        FinalExample obj = new FinalExample();
+        System.out.println("Number: " + obj.number);
+    }
+}
